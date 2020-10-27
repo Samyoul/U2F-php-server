da93e90a7085a3253b2877c12dc0fba6a4237a71
William Desportes <williamdes@wdes.fr>
2020-10-27 23:50:10 +0100

    Add support for phpunit

---
+++ test/VoidTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Samyoul\U2F\U2FServer\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class VoidTest extends TestCase
+{
+    public function testNothing(): void
+    {
+        $this->assertTrue(true);
+    }
+}
