b21bc14aabd6d0b7aed672e3feef11f7ca40ec3f
William Desportes <williamdes@wdes.fr>
2020-10-30 16:55:57 +0100

    Add a test case for #6

---
+++ test/U2FServerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Samyoul\U2F\U2FServer\Tests;
+
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+use Samyoul\U2F\U2FServer\U2FServer;
+
+class U2FServerTest extends TestCase
+{
+    protected static function getClassMethod(string $className, string $methodName) {
+        $class = new ReflectionClass($className);
+        $method = $class->getMethod($methodName);
+        $method->setAccessible(true);
+        return $method;
+    }
+
+    public function testCreateChallenge(): void
+    {
+        $foo = self::getClassMethod(U2FServer::class, 'createChallenge');
+        $challengeResult = $foo->invokeArgs(new U2FServer(), []);
+        $this->assertMatchesRegularExpression('/^[a-z0-9_-]{20,}$/i', $challengeResult);
+    }
+}
